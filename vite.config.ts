d329d1fc4c9b114b4f8b3734d7fad9e7d3126a41
mhrastegari <mh.rastegari@outlook.com>
2024-11-18 19:39:08 +0330

    Add VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const baseUrl = mode === "production" ? "/react-pwa-test/" : "/";
+  return {
+    base: baseUrl,
+    server: {
+      port: 5654,
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        scope: baseUrl,
+        manifest: {
+          name: "React PWA Test",
+          short_name: "ReactPWATest",
+          description: "A React app to test PWA behaviors",
+          theme_color: "#ffffff",
+          background_color: "#ffffff",
+          display: "standalone",
+          orientation: "portrait",
+          start_url: "/",
+          icons: [
+            {
+              src: "./icons/icon-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "./icons/icon-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
