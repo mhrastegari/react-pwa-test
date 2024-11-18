42ed3eb2e796978cf5482d810c9d7ea20f835fe0
mhrastegari <mh.rastegari@outlook.com>
2024-11-18 20:18:41 +0330

    Fix PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -26,12 +26,12 @@ export default defineConfig(({ mode }) => {
           start_url: "/",
           icons: [
             {
-              src: "./icons/icon-192x192.png",
+              src: "icons/icon-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "./icons/icon-512x512.png",
+              src: "icons/icon-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
